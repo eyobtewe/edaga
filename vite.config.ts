1b41a458b0ae56a6d8a4bf1e22f9aefc81ee825e
Eyob Tewelde <51155116+eyobtewe@users.noreply.github.com>
2025-01-31 08:37:53 -0600

    Fix MIME type issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: "esnext", // Ensures modern JavaScript is used
+    target: "esnext",
     assetsInlineLimit: 0,
     rollupOptions: {
       output: {
